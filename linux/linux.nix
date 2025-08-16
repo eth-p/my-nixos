5089467f2f06b24277d9a6b59978cfe114b29b67
Ethan P. <eth-p+git@hidden.email>
2025-08-15 20:28:46 -0700

    feat(linux): Option to suppress kernel log messages in VTs

--- linux/linux.nix
+++ linux/linux.nix
@@ -5,7 +5,7 @@
 # ==============================================================================
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) mkMerge;
+  inherit (lib) mkMerge mkIf;
   cfg = config.my-nixos.linux;
 in {
   options.my-nixos.linux = {
@@ -13,6 +13,13 @@ in {
       type = lib.types.enum [ "latest" "cachyos-zen4" ];
       default = "latest";
       description = "the Linux kernel packages";
+    };
+
+    quiet-consoles = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "suppress kernel messages on the specified TTYs";
+      example = ''[ "tty2" "tty3" ]'';
     };
   };
 
@@ -25,18 +32,34 @@ in {
         "cachyos-zen4" =
           pkgs.linuxPackages_cachyos.cachyOverride { mArch = "ZEN4"; };
       };
-    in {
-      boot.kernelPackages = kernels."${cfg.kernel}";
-    })
+    in { boot.kernelPackages = kernels."${cfg.kernel}"; })
 
     # Install useful tools.
     {
-      environment.systemPackages = with pkgs; [
-        usbutils
-        pciutils
-        file
-      ];
+      environment.systemPackages = with pkgs; [ usbutils pciutils file ];
     }
+
+    # Enable features required for unlocking volumes via TPM 2.0.
+    # Before enabling this, you must manually enroll the keys:
+    #   sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /dev/the-disk
+    (mkIf ((builtins.length cfg.quiet-consoles) > 0) {
+      systemd.services.quiet-consoles = {
+        description = "Suppress kernel messages on specific consoles";
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = ''
+            /bin/sh -c 'for tty in ${
+              pkgs.lib.escapeShellArgs cfg.quiet-consoles
+            }; do \
+              if [ -e "/dev/$tty" ]; then \
+                /run/current-system/sw/bin/setterm -msg off -term linux <"/dev/$tty" >"/dev/$tty"; \
+              fi \
+            done'
+          '';
+        };
+      };
+    })
 
   ];
 }
