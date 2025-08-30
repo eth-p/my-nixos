87e974eaf91868f616e0fa4424777bdf5d7683ad
Ethan P. <eth-p+git@hidden.email>
2025-08-29 20:50:43 -0700

    feat(gpu-screen-recorder): Update for new UI
    Thanks to @keenanweaver (https://github.com/keenanweaver) for
    creating the overlays.

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,6 +5,9 @@
 # ==============================================================================
 inputs: (final: prev: {
 
+  gpu-screen-recorder = prev.callPackage ./gpu-screen-recorder.nix {};
+  gpu-screen-recorder-ui = prev.callPackage ./gpu-screen-recorder-ui.nix {};
+  gpu-screen-recorder-notification = prev.callPackage ./gpu-screen-recorder-notification.nix {};
   kde-theme-vinyl = prev.callPackage ./kde-theme-vinyl.nix {};
 
 })
--- programs/gpu-screen-recorder.nix
+++ programs/gpu-screen-recorder.nix
@@ -9,26 +9,66 @@
   config,
   lib,
   pkgs,
-  my-nixos,
   ...
 }:
 let
-  inherit (lib) mkIf mkMerge mkDefault;
+  inherit (lib) mkMerge mkIf;
   cfg = config.my-nixos.programs.gpu-screen-recorder;
 in
 {
   options.my-nixos.programs.gpu-screen-recorder = {
     enable = lib.mkEnableOption "install gpu-screen-recorder";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "The package for gpu-screen-recorder.";
+      default = pkgs.gpu-screen-recorder;
+    };
+
+    ui.enable = lib.mkEnableOption "install a UI for gpu-screen-recorder" // {
+      default = true;
+    };
+
+    ui.package = lib.mkOption {
+      type = lib.types.package;
+      description = "The UI package for gpu-screen-recorder.";
+      default = pkgs.gpu-screen-recorder-ui;
+    };
   };
 
   config = lib.mkIf cfg.enable (mkMerge [
 
     {
-      programs.gpu-screen-recorder.enable = true;
+      security.wrappers = {
+        "gsr-kms-server" = {
+          owner = "root";
+          group = "root";
+          capabilities = "cap_sys_admin+ep";
+          source = lib.getExe' cfg.package "gsr-kms-server";
+        };
+      };
+
       environment.systemPackages = with pkgs; [
-        gpu-screen-recorder-gtk
+        gpu-screen-recorder
       ];
     }
+
+    (mkIf cfg.ui.enable {
+      security.wrappers = {
+        "gsr-global-hotkeys" = {
+          owner = "root";
+          group = "root";
+          capabilities = "cap_setuid+ep";
+          source = lib.getExe' cfg.ui.package "gsr-global-hotkeys";
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        gpu-screen-recorder-ui
+        gpu-screen-recorder-notification
+      ];
+
+    })
 
   ]);
 }
