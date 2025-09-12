fe6f482c6e24ce5510f3942430ac9c56b10e8c8a
Ethan P. <eth-p+git@hidden.email>
2025-09-12 01:58:01 -0700

    upgrade(nvidia): Drivers 580.82.09

--- patches/linux-nvidia-580.nix
+++ patches/linux-nvidia-580.nix
@@ -1,18 +1,18 @@
 # my-nixos | Copyright (C) 2025 eth-p
 # Repository: https://github.com/eth-p/my-nixos
 #
-# The nvidia-575 driver builds are broken and need a patch.
+# The nvidia-580 driver builds are broken and need a patch.
 # https://github.com/NixOS/nixpkgs/issues/412299#issuecomment-2955980698
 # ==============================================================================
 { config, pkgs, ... }:
 let
 in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-  version = "580.82.07";
-  sha256_64bit = "sha256-Bh5I4R/lUiMglYEdCxzqm3GLolQNYFB0/yJ/zgYoeYw=";
-  sha256_aarch64 = "sha256-or3//aV4TQcPDgcLxFB75H/kB8n+3RzwTO1C2ZbJAJI=";
-  openSha256 = "sha256-8/7ZrcwBMgrBtxebYtCcH5A51u3lAxXTCY00LElZz08=";
-  settingsSha256 = "sha256-lx1WZHsW7eKFXvi03dAML6BoC5glEn63Tuiz3T867nY=";
-  persistencedSha256 = "sha256-1JCk2T3H5NNFQum0gA9cnio31jc0pGvfGIn2KkAz9kA=";
+  version = "580.82.09";
+  sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+  sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+  openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+  settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+  persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
   usePersistenced = true;
   patches = [];
 }
