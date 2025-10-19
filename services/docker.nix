e768ebb8223dce228405b5c16ce784d915d57890
Ethan P. <eth-p+git@hidden.email>
2025-10-19 15:25:12 -0700

    feat(services/docker): Add trusted-users option

--- services/docker.nix
+++ services/docker.nix
@@ -13,6 +13,12 @@ in {
   options.my-nixos.services.docker = {
     enable = lib.mkEnableOption "install Docker";
     rootless = lib.mkEnableOption "use Docker rootless" // { default = true; };
+
+    trusted-users = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "users allowed to communicate with the Docker daemon";
+      default = [];
+    };
   };
 
   config = lib.mkIf cfg.enable (mkMerge [
@@ -25,5 +31,10 @@ in {
         setSocketVariable = true;
       };
     })
+
+    # Add trusted users to the Docker group.
+    {
+      users.groups.docker.members = cfg.trusted-users;
+    }
   ]);
 }
