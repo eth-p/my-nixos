6d3a3788e978b125a22a88bca81f14e4b596e74e
Ethan P. <eth-p+git@hidden.email>
2025-12-17 00:00:55 -0800

    build: Recreate devenv config

--- devenv.nix
+++ devenv.nix
@@ -1,36 +1,69 @@
-# my-dotfiles | Copyright (C) 2025 eth-p
-# Repository: https://github.com/eth-p/my-dotfiles
+# my-nixos | Copyright (C) 2025 eth-p
+# Repository: https://github.com/eth-p/my-nixos
 #
 # This provides a consistent environment for running development scripts.
 # ==============================================================================
-{ pkgs, lib, config, inputs, ... }:
 {
-  packages = [
-    pkgs.git
-
-    # Formatters
-    pkgs.treefmt
-    pkgs.shfmt
-    pkgs.nixpkgs-fmt
-  ];
-
-  # https://devenv.sh/tasks/
-  tasks = {
-    "my-nixos:format" = {
-      exec = "treefmt";
-    };
-
-    "my-nixos:docs" = {
-      exec = ''
-        nix run github:Thunderbottom/nix-options-doc?rev=2caa4b5756a8666d65d70122f413e295f56886e7 -- \
-          --path "." \
-          --out OPTIONS.md \
-          --exclude-dir "lib" \
-          --exclude-dir "profiles" \
-          --filter-by-prefix "options.my-dotfiles" \
-          --strip-prefix "options." \
-          --sort
-      '';
-    };
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  options.fast = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
   };
+
+  config = lib.mkMerge [
+    {
+      packages = [
+        pkgs.git
+
+        # Formatters
+        pkgs.treefmt
+        pkgs.nodePackages.prettier
+        pkgs.shfmt
+        pkgs.nixfmt-rfc-style
+        pkgs.stylua
+      ];
+
+      # https://devenv.sh/tasks/
+      tasks = {
+        "my-nixos:format" = {
+          exec = "treefmt";
+        };
+      };
+    }
+
+    # Configuration applied when not using direnv.
+    (lib.mkIf (!config.fast) (
+      let
+        system = pkgs.stdenv.system;
+      in
+      {
+        packages = [
+          inputs.nix-options-doc.packages.${system}.default
+        ];
+
+        # https://devenv.sh/tasks/
+        tasks = {
+          "my-nixos:docs" = {
+            before = [ "my-nixos:format" ];
+            exec = ''
+              nix-options-doc \
+                --path "." \
+                --out OPTIONS.md \
+                --exclude-dir "lib" \
+                --exclude-dir "profiles" \
+                --filter-by-prefix "options.my-nixos" \
+                --strip-prefix "options." \
+                --sort
+            '';
+          };
+        };
+      }
+    ))
+  ];
 }
