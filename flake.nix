1d97c854efca40d11b31e6288a66a9daec39118b
Ethan P. <eth-p+git@hidden.email>
2026-04-15 19:37:29 -0700

    upgrade(lanzaboote): v1.0.0 -> v1.0.0-94-g18e9e64

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-cachyos-kernel = {
