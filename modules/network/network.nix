9878c04a5e754e23ebd407b58544ae09a255a656
Ethan P. <eth-p+git@hidden.email>
2026-06-29 14:33:45 -0700

    fix(network): Workaround 2+ NICs on same network

--- modules/network/network.nix
+++ modules/network/network.nix
@@ -9,7 +9,18 @@ let
   cfg = config.my-nixos.network;
 in
 {
-  options.my-nixos.network = { };
+  options.my-nixos.network = {
+    dual-lan = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = ''
+          Enable if the device will connect to the same network over
+          multiple interfaces.
+        '';
+      };
+    };
+  };
 
   config = mkMerge [
 
@@ -19,6 +30,16 @@ in
       # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
       networking.networkmanager.enable = true;
     }
+
+    # If dual-LAN is enabled, only reply to ARPs on matching interfaces.
+    # This is necessary to prevent IP address conflicts when both interfaces
+    # are connected to the same network.
+    (lib.mkIf cfg.dual-lan.enable {
+      boot.kernel.sysctl = {
+        "net.ipv4.conf.all.arp_ignore" = 1;
+        "net.ipv4.conf.all.arp_announce" = 2;
+      };
+    })
 
   ];
 }
